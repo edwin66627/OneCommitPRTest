9e1fcd5cb7f1976107144b11e1bf0cb22cda1675
Edwin Erley Quevedo Sierra <edwinqs1956@hotmail.com>
2025-10-30 11:53:40 -0500

    Revert "Merge pull request #4 from edwin66627/feature-branch-two"
    This reverts commit 85c54c9fe2691e9b25ce299ba5b53d6a9a0dc3e6, reversing
    changes made to cae210277e6d47037e7f80f153ef21186d4e400e.

--- src/Main.java
+++ src/Main.java
@@ -7,11 +7,13 @@ public class Main {
 
         System.out.print("Hello and welcome! \n");
 
+        System.out.println("Ascending loop operation");
         for (int i = 1; i <= upperLimit; i++) {
             System.out.println("Number = " + i);
             System.out.println("Number multiplied = " + i * 2);
         }
 
+        System.out.println("Descending loop operation");
         for (int i = 100; i >= lowerLimit; i--) {
             System.out.println("Number = " + i);
             System.out.println("Number multiplied = " + i * 2);
